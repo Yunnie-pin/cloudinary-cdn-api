f5a5e352e321d63e5ec4f0ffcf63fe7da4e75593
niwa <niwaisme@proton.me>
2024-08-15 10:07:03 +0700

    feat: change index.html with repo desc and contribs

--- router/router.go
+++ router/router.go
@@ -3,9 +3,16 @@ package router
 import (
 	"claudinary-cdn-api/controllers"
 	"claudinary-cdn-api/middlewares"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
+
+var repoData map[string]interface{} = nil
+var contribData []map[string]interface{} = nil
 
 func NewRouter(
 	bucketController *controllers.BucketController,
@@ -18,7 +25,59 @@ func NewRouter(
 
 	//web
 	r.GET("/", func(c *gin.Context) {
-		c.HTML(200, "index.html", nil)
+		client := &http.Client{}
+		method := "GET"
+
+		if repoData == nil {
+			url := "https://api.github.com/repos/Yunnie-pin/cloudinary-cdn-api"
+			req, err := http.NewRequest(method, url, nil)
+			if err != nil {
+				fmt.Printf("Error: %v", err)
+			}
+			req.Header.Add("Content-Type", "application/json")
+
+			res, err := client.Do(req)
+			if err != nil {
+				fmt.Printf("Error: %v", err)
+			}
+			defer res.Body.Close()
+			body, err := io.ReadAll(res.Body)
+
+			repoData = make(map[string]interface{})
+			_ = json.Unmarshal(body, &repoData)
+
+			if err != nil {
+				fmt.Printf("Error: %v", err)
+			}
+		}
+		if contribData == nil {
+			url := "https://api.github.com/repos/Yunnie-pin/cloudinary-cdn-api/contributors"
+			req, err := http.NewRequest(method, url, nil)
+			if err != nil {
+				fmt.Printf("Error: %v", err)
+			}
+			req.Header.Add("Content-Type", "application/json")
+
+			res, err := client.Do(req)
+			if err != nil {
+				fmt.Printf("Error: %v", err)
+			}
+			defer res.Body.Close()
+
+			body, err := io.ReadAll(res.Body)
+
+			contribData = make([]map[string]interface{}, 0)
+			_ = json.Unmarshal(body, &contribData)
+
+			if err != nil {
+				fmt.Printf("Error: %v", err)
+			}
+		}
+
+		c.HTML(200, "index.html", gin.H{
+			"repo":         repoData,
+			"contributors": contribData,
+		})
 	})
 
 	r.Use(middlewares.CORSMiddleware())
